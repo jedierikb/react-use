21db557d5f3d91aa145a6658e7b09ec44c0f9be7
Erik Blankinship <erikb@mediamods.com>
2021-09-27 19:37:11 -0400

    no message

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from '@rollup/plugin-babel';
+import {
+  terser
+} from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default [
+  {
+    input: pkg.module,
+    output: {
+      file: 'doc/react-use.js',
+      format: 'esm'
+    },
+    plugins: [
+      babel({
+        "exclude": 'node_modules/**',
+        "babelHelpers": "bundled",
+        "presets": [
+          "@babel/preset-env"
+        ]
+      }),
+      terser()
+    ],
+    external: [
+      'react',
+      'react-dom'
+    ]
+  },
+];
